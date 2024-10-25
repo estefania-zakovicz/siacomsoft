ac597d7ae3cd889e4b891d6e6cdcc12194a614fe
estefania-zakovicz <estefaniazakovicz@hotmail.com>
2024-10-24 22:44:25 -0300

    qué hacemos

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Header from "./Components/Header/Header";
 import Hero_animation from "./Components/Hero/Hero_animation";
 import Content from "./Components/Hero/Content";
+import Make from "./Components/Make/Make";
 
 const App = () => {
   const [showContent, setShowContent] = useState(false);
@@ -11,11 +12,14 @@ const App = () => {
   };
 
   return (
-    <div className="relative h-screen w-full overflow-hidden">
+    <div className="relative overflow-hidden">
       <Header />
-      <div className="relative h-full w-full">
+      <div className="h-screen w-full">
         <Hero_animation onShowContent={handleShowContent} />
         {showContent && <Content />}
+      </div>
+      <div className="h-screen w-full">
+        <Make />
       </div>
     </div>
   );
